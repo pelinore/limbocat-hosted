410a1340478b0dba9f26519d3ba86a6db8fadcd0
Brett Crocker <brettc@limbocat.com>
2011-12-23 02:48:16 -0600

    Added wordpress::deploy recipe to the role Changed the version/checksum to the 3.3 values, since that's been released.

--- roles/wordpress.rb
+++ roles/wordpress.rb
@@ -1,8 +1,8 @@
 name "wordpress"
 description "Blog using wordpress"
 recipes "apt", "build-essential", "chef-client::service", "users::sysadmins",
-        "sudo", "postfix", "mysql::server", "wordpress", "wordpress::blog_user",
-        "wordpress::add_swap", "vsftpd"
+        "sudo", "postfix", "mysql::server", "wordpress", "wordpress:: deploy",
+        "wordpress::blog_user", "wordpress::add_swap", "vsftpd"
 
 override_attributes(
   "postfix" => {"myhostname" => "test.limbocat.com", "mydomain" => "limbocat.com"},
@@ -14,8 +14,8 @@ override_attributes(
   },
   "wordpress" => {
      "server_aliases" => %w(test.limbocat.com),
-     "version" => "3.2.1",
-     "checksum" => "21e3cebd02808f9ee39a979d22e6e10bce5356ddf7068aef182847b12c9b95a9",
+     "version" => "3.3",
+     "checksum" => "ba32a18d3fe263cbe095d846817173b808a74a497c3eab016149cdf8fcbb3e43",
      "weblog_title" => "LimbocatBlog",
      "user_name" => "brettc",
      "admin_email" => "brettc@limbocat.com",
